23c93d878e9cc28329c306f0cd2c1ba59008fa10
toritheterrible <torimpham@gmail.com>
2020-09-29 16:55:55 -0700

    pushed to heroku and changed to atlas

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -16,12 +20,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cookitrecipes", {
+mongoose.connect(process.env.REACT_APP_MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
