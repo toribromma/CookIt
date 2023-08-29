1a60510c96ff9a32189d38c80f69d787dec7e367
Tori Bromma <toribromma@gmail.com>
2023-08-28 17:49:39 -0700

    removed lazy loading

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,18 +1,18 @@
-import React from "react"
+import React from "react";
 
 import logo from "../../images/logo.jpg";
 
 export default function Logo(props) {
-    return(
-        <img 
-        src={logo} 
-        alt={props.alt}
-        style={{
-            display: "block",
-            width: props.width,
-            margin: "30px auto",
-            borderRadius: "200px",
-        }}
-        ></img>
-    )
+  return (
+    <img
+      src={logo}
+      alt={props.alt}
+      style={{
+        display: "block",
+        width: props.width,
+        margin: "30px auto",
+        borderRadius: "200px",
+      }}
+    ></img>
+  );
 }
