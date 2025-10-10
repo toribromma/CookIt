3a770efc484834b166ed5976c2a596ef2ff846d1
Tori Bromma <toribromma@gmail.com>
2025-10-10 13:23:50 -0700

    Update [id].js

--- api/recipes/[id].js
+++ api/recipes/[id].js
@@ -1,15 +1,18 @@
-import connectDB from "../../config/db.js";
+// /api/recipes/[id].js
+import dbConnect from "../../config/db.js";
 import Recipe from "../../models/Recipe.js";
 
 export default async function handler(req, res) {
-  await connectDB();
   const { id } = req.query;
+
+  await dbConnect();
 
   try {
     const recipe = await Recipe.findById(id);
     if (!recipe) return res.status(404).json({ error: "Recipe not found" });
     res.status(200).json(recipe);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to load recipe" });
+  } catch (error) {
+    console.error("Fetch recipe error:", error);
+    res.status(500).json({ error: "Failed to fetch recipe" });
   }
 }
