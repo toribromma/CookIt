e420e7eea0af1413d5fb7570dcd9b324a8f42e9f
Tori Bromma <toribromma@gmail.com>
2025-10-10 12:59:17 -0700

    Update saved.js

--- api/recipes/saved.js
+++ api/recipes/saved.js
@@ -1,39 +1,25 @@
 import connectDB from "../../config/db.js";
 import Recipe from "../../models/Recipe.js";
 
+connectDB();
+
 export default async function handler(req, res) {
-  await connectDB();
-
-  switch (req.method) {
-    case "GET":
-      try {
-        const recipes = await Recipe.find({});
-        res.status(200).json(recipes);
-      } catch (err) {
-        res.status(500).json({ error: "Failed to get saved recipes" });
-      }
-      break;
-
-    case "POST":
-      try {
-        const newRecipe = await Recipe.create(req.body);
-        res.status(201).json(newRecipe);
-      } catch (err) {
-        res.status(500).json({ error: "Failed to save recipe" });
-      }
-      break;
-
-    case "DELETE":
-      try {
-        const { id } = req.query;
-        await Recipe.findByIdAndDelete(id);
-        res.status(200).json({ message: "Recipe deleted" });
-      } catch (err) {
-        res.status(500).json({ error: "Failed to delete recipe" });
-      }
-      break;
-
-    default:
-      res.status(405).json({ error: "Method not allowed" });
+  if (req.method === "GET") {
+    try {
+      const recipes = await Recipe.find();
+      res.status(200).json(recipes);
+    } catch (err) {
+      res.status(500).json({ error: "Failed to get saved recipes" });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const newRecipe = await Recipe.create(req.body);
+      res.status(201).json(newRecipe);
+    } catch (err) {
+      res.status(500).json({ error: "Failed to save recipe" });
+    }
+  } else {
+    res.setHeader("Allow", ["GET", "POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
