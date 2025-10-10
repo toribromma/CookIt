071dc06cccbc6fbbbdd335850ff886136b424015
Tori Bromma <toribromma@gmail.com>
2025-10-10 10:23:16 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if (!global.mongooseConnection) {
-    global.mongooseConnection = mongoose.connect(process.env.MONGODB_URI, {
+  if (mongoose.connections[0].readyState) return; // reuse existing connection
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
   }
-  return global.mongooseConnection;
 };
 
 export default connectDB;
