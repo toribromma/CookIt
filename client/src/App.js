50ec0fae51f67cdb020aad53a45b9ecf50a5767b
Tori Bromma <toribromma@gmail.com>
2025-10-10 11:54:15 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,11 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-import "./App.css";
-import Logo from "./components/Logo/Logo";
-import Header from "./components/Header/Header";
-import Login from "./pages/Login";
-import MainPage from "./pages/main";
-import Detail from "./pages/Detail";
-import NewRecipePage from "./components/SearchRecipeContainer/NewRecipePage";
-import NavBar from "./components/Navbar/Navbar";
-import SearchRecipeContainer from "./components/SearchRecipeContainer/SearchRecipeContainer";
-import ExtractRecipeContainer from "./components/ExtractRecipeContainer/ExtractRecipeContainer";
+import { RecipeProvider } from "./contexts/RecipeContext";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  const { user, isAuthenticated } = useAuth0();
-
   return (
-    <Router>
-      {user && <NavBar></NavBar>}
-
-      <Header color={"#e63946"}>
-        <Logo width={"200px"} alt="panda chef hat"></Logo>
-      </Header>
-      <Routes>
-        <Route exact path="/" element={<Login />}></Route>
-        <Route path="/main" element={<MainPage />}></Route>
-        <Route path="/searchRecipe" element={<SearchRecipeContainer />}></Route>
-        <Route
-          path="/extractRecipe"
-          element={<ExtractRecipeContainer />}
-        ></Route>
-        <Route exact path="/main/recipe/:id" element={<Detail />}></Route>
-        <Route
-          exact
-          path="/searchRecipe/recipe/new/:id"
-          element={<NewRecipePage />}
-        ></Route>
-      </Routes>
-    </Router>
+    <RecipeProvider>
+      <MainPage />
+    </RecipeProvider>
   );
 }
 
