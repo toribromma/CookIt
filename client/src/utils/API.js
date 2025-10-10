b28a241858c2b9d650ff7156efdc8f5b251ce312
Tori Bromma <toribromma@gmail.com>
2025-10-10 11:32:04 -0700

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,60 +1,22 @@
-const API_URL = "/api/recipes";
+// utils/API.js
 
-export default {
-  getRecipes: async (query) => {
-    const res = await fetch(`${API_URL}?q=${encodeURIComponent(query || "")}`);
-    return res.json();
-  },
-
-  getRecipe: async (id) => {
-    const res = await fetch(`${API_URL}/${id}`);
-    return res.json();
-  },
-
-  saveRecipe: async (recipeData) => {
-    const res = await fetch(API_URL, {
+const API = {
+  // DB calls (saved recipes)
+  getRecipes: () => fetch("/api/recipes").then(res => res.json()),
+  saveRecipe: (recipe) =>
+    fetch("/api/recipes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(recipeData),
-    });
-    return res.json();
-  },
+      body: JSON.stringify(recipe),
+    }),
+  deleteRecipe: (id) =>
+    fetch(`/api/recipes/${id}`, { method: "DELETE" }),
 
-  deleteRecipe: async (id) => {
-    const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
-    return res.json();
-  },
-
-  updateRecipeTitle: async (recipeData) => {
-    const res = await fetch(`${API_URL}/${recipeData.id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: recipeData.title,
-        ingredients: recipeData.ingredients,
-        instructions: recipeData.instructions,
-        cuisine: recipeData.cuisine,
-      }),
-    });
-    return res.json();
-  },
-
-  searchRecipes: async (id) => {
-    const res = await fetch(`${API_URL}/search/${id}`);
-    return res.json();
-  },
-
-  getNewRecipe: async (id) => {
-    const res = await fetch(`${API_URL}/new/search/${id}`);
-    return res.json();
-  },
-
-  saveNewRecipe: async (recipeData) => {
-    const res = await fetch(`${API_URL}/new`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(recipeData),
-    });
-    return res.json();
-  },
+  // Spoonacular calls (external)
+  searchRecipes: (query) =>
+    fetch(`/api/external/recipes/search?query=${query}`).then(res => res.json()),
+  getRecipe: (id) =>
+    fetch(`/api/external/recipes/${id}`).then(res => res.json()),
 };
+
+export default API;
